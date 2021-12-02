d9d4286aca32b904daceefcfa5a88d5d0ef86f89
John Rinehart <johnrichardrinehart@gmail.com>
2021-12-02 14:58:58 +0200

    feat(/1/1): open and read input file

--- 1/1/src/main.rs
+++ 1/1/src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::io::{BufReader, BufRead};
+use std::fs::File;
+
 fn main() {
     println!("Hello World!");
+    let input_path = ::std::env::args().nth(1).unwrap();
+    let handle = File::open(&input_path);
+    match handle {
+        Err(ref e) => {
+            println!("failed to open file: {:?}", e);
+            return
+        },
+        Ok(ref v) => println!("opened it! {:?}", v),
+    }
+    let unwrapped = handle.unwrap();
+    let file = BufReader::new(unwrapped);
+    for line in file.lines() {
+        let my_line = line.unwrap();
+        println!("{}", my_line);
+    }
 }
+
